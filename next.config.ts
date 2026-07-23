66af5a7182e5fd05e4100bc361b4a135811ef3ca
madoyakimberley <kimmadoya06@gmail.com>
2026-07-23 22:07:51 +0300

    Add yt-search and cheerio to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["yt-search", "cheerio"],
 };
 
 export default nextConfig;
