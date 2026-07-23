9b7986d7c229342fe478b627fa1bae4f2c5e80dd
madoyakimberley <kimmadoya06@gmail.com>
2026-07-23 22:53:15 +0300

    Fix: include cheerio* wildcard for cheerio-select

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     "/api/search": [
-      "./node_modules/cheerio/**/*",
+      "./node_modules/cheerio*/**/*",
       "./node_modules/dom*/**/*",
       "./node_modules/htmlparser2/**/*",
       "./node_modules/css-select/**/*",
