0d7c615d3332b369042abf82826d7ebb52f3fd21
madoyakimberley <kimmadoya06@gmail.com>
2026-07-23 22:56:32 +0300

    Fix: force bundle entire node_modules for search API

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
-    "/api/search": [
-      "./node_modules/cheerio*/**/*",
-      "./node_modules/dom*/**/*",
-      "./node_modules/htmlparser2/**/*",
-      "./node_modules/css-select/**/*",
-      "./node_modules/entities/**/*",
-      "./node_modules/boolbase/**/*",
-      "./node_modules/nth-check/**/*",
-      "./node_modules/parse5/**/*",
-    ],
+    "/api/search": ["./node_modules/**/*"],
   },
 };
 
