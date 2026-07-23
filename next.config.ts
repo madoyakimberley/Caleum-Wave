e780db48df99de87735b4fea8024c5fef2401476
madoyakimberley <kimmadoya06@gmail.com>
2026-07-23 22:47:29 +0300

    Fix: trace cheerio sub-dependencies cleanly in next.config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
-    "/api/search": ["./node_modules/cheerio/**/*"],
+    "/api/search": [
+      "./node_modules/cheerio/**/*",
+      "./node_modules/domutils/**/*",
+      "./node_modules/htmlparser2/**/*",
+      "./node_modules/css-select/**/*",
+      "./node_modules/domhandler/**/*",
+      "./node_modules/dom-serializer/**/*",
+      "./node_modules/entities/**/*",
+    ],
   },
 };
 
